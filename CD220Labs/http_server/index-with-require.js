12360e7a9031c43fbe6071469337705cae79e8b2
Johnson Prasad <jtprasad@ymail.com>
2026-01-29 18:11:27 -0700

    updated to return greeting, date, and time

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -4,7 +4,15 @@ const today = require('./today');                                               
 const requestListener = function (req, res) {                                      // Define request listener function
     res.writeHead(200);                                                            // Set status code to 200
     let dateVal = today.getDate();                                                 // Get current date from 'today'
-    let greeting = "It is still not morning";                                      // Determine appropriate greeting based on current time
+    let azTime = dateVal.toLocaleTimeString("en-US");
+    let dateString = dateVal.toLocaleDateString('en-US', {                         // Get current date format 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric'
+    });
+    let hour = dateVal.getHours();
+    let greeting = "";                                                             // Determine appropriate greeting based on current time
     if (dateVal.getHours() > 6 && dateVal.getHours() < 12) {
         greeting = "Good morning!";
     } else if (dateVal.getHours() >= 12 && dateVal.getHours() < 18) {
@@ -14,7 +22,7 @@ const requestListener = function (req, res) {                                   
     } else if (dateVal.getHours() >= 21 && dateVal.getHours() < 24) {
         greeting = "Good night!";
     }
-    res.end(`Hello, ${greeting}`);                                                 // Send response with appropriate greeting
+    res.end(`${greeting} Today is ${dateString} and the time is ${azTime}`);       // Send response with appropriate greeting
 };
 
 const port = 8080;                                                                 // Define port number
