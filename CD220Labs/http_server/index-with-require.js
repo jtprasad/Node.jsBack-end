0eddc53a4fae98054df08456067f3b38e278e6fb
Johnson Prasad <jtprasad@ymail.com>
2026-01-29 17:14:35 -0700

    Updated index-with-require.js

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,22 +1,25 @@
- // Import the HTTP module
-const http = require('http');
+const http = require('http');                                                      // Import the HTTP module
+const today = require('./today');                                                  // Import the 'today' module
 
-// Import the 'today' module
-const today = require('./today');
-
-// Define the request listener function
-const requestListener = function (req, res) {
-    res.writeHead(200); // Set the status code to 200 (OK)
-    // Send the response with the current date from the 'today' module
-    res.end(`Hello, World! The date today is ${today.getDate()}`);
+const requestListener = function (req, res) {                                      // Define the request listener function
+    res.writeHead(200);                                                            // Set the status code to 200
+    let dateVal = today.getDate();                                                 // Get the current date from the 'today' module
+    let greeting = "It is still not morning";                                      // Determine the appropriate greeting based on the current time
+    if (dateVal.getHours() > 6 && dateVal.getHours() < 12) {
+        greeting = "Good morning!";
+    } else if (dateVal.getHours() >= 12 && dateVal.getHours() < 18) {
+        greeting = "Good afternoon!";
+    } else if (dateVal.getHours() >= 18 && dateVal.getHours() < 21) {
+        greeting = "Good evening!";
+    } else if (dateVal.getHours() >= 21 && dateVal.getHours() < 24) {
+        greeting = "Good night!";
+    }
+    res.end(`Hello, ${greeting}`);                                                 // Send the response with the appropriate greeting
 };
 
-// Define the port number
-const port = 8080;
+const port = 8080;                                                                 // Define the port number
 
-// Create an HTTP server using the request listener function
-const server = http.createServer(requestListener);
+const server = http.createServer(requestListener);                                 // Create an HTTP server using the request listener function
 
-// Start the server and listen on the specified port
-server.listen(port);
+server.listen(port);                                                               // Start the server and listen on the specified port
 console.log('Server listening on port: ' + port);
